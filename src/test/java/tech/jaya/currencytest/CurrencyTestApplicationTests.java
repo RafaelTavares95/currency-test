9ae9a5c1877aa6c3f5e70e3e7dcce94c0797bf52
Rafael Tavares <rafaeltavares.rofino198@gmail.com>
2020-11-03 19:54:53 -0300

    starting project

---
+++ src/test/java/tech/jaya/currencytest/CurrencyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.jaya.currencytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
